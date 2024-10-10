25049f0d5b7f2a3c9cfd725fabbe022466a9b961
Rishub Kumar <itsrishub@gmail.com>
2024-10-10 13:12:49 +0530

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 
 def getcsrf():
-    return BeautifulSoup(requests.get('https://ride.guru').content, 'html.parser').find('input', {'name': 'csrfmiddlewaretoken'})['value']
+    # return BeautifulSoup(requests.get('https://ride.guru').content, 'html.parser').find('input', {'name': 'csrfmiddlewaretoken'})['value']
+    session = requests.Session()
+    session.get('https://ride.guru')
+    return session.cookies.get_dict()
 
 def search(query: str):
     return requests.get(f'https://api.mapbox.com/geocoding/v5/mapbox.places/{query}.json?types=district,postcode,place,neighborhood,address,poi&autocomplete=true&limit=10&proximity=77.663587,12.994375&access_token=pk.eyJ1IjoiY2hyaXNoYXdlcyIsImEiOiJjbGNpbGV0dmc0M21xM29tb2syZHE3YmU4In0.Vse1fk7Oga4WpeJsWIH28w').json()
@@ -21,7 +24,8 @@ def read_root():
 
 @app.get("/get")
 def read_item():
-    return {"csrf": getcsrf()}
+    # return {"csrf": getcsrf()}
+    return getcsrf()
 
 @app.get("/search/{query}")
 def read_item(query: str):
